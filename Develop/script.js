f1d27d77e1efaa02b3f23d1b1b5a14baeda0abb8
Tara Evans <tarajevans@hotmail.com>
2022-07-10 20:53:52 -0400

    saved to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -2,17 +2,6 @@
 var saveBtn=document.getElementById("saveBtn");
 var timeBlocks = [];
 var currentDate;
-var tasks = [
-    { hour: "09", task: "",},
-    { hour: "10", task: "",},
-    { hour: "11", task: "",},
-    { hour: "12", task: "",},
-    { hour: "13", task: "",},
-    { hour: "14", task: "",},
-    { hour: "15", task: "",},
-    { hour: "16", task: "",},
-    { hour: "17", task: "",}
-];
 var currentDate;
 
 //displays date in header
@@ -28,15 +17,12 @@ function displayCurrentDate(){
     document.getElementById("currentDay").innerHTML = currentDate;
 }
 
-function saveToStorage() {
-    localStorage.setItem("hourlyTask", JSON.stringify(tasks));
-}
-
 $(".container").on("click", "button", function(event) {
     $.each(timeBlocks, function(index, timeBlock){
         if(timeBlock.dataset.hour === event.target.parentNode.id){
             var taskText = $(timeBlock).val();
-            console.log(taskText);
+            localStorage.setItem("hour"+timeBlock.dataset.hour, taskText);
+           console.log(localStorage.getItem("hour"+ timeBlock.dataset.hour));
         }
     })
 })
